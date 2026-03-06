995bea41e311b11f636d0b9a39a348e46a599984
zub <ars1for1work@gmail.com>
2026-03-06 20:19:58 +0300

    Fix CG

--- interface.hpp
+++ interface.hpp
@@ -1,13 +1,16 @@
 #include <iostream>
-template <class T> struct BiList {
+template < class T >
+struct BiList
+{
   T val;
-  BiList<T> *next;
-  BiList<T> *prev;
+  BiList< T >* next;
+  BiList< T >* prev;
 };
 
-template <class T> // добавить перед
-BiList<T> *add(BiList<T> *h, const T &v) {
-  BiList<T> *e = new BiList<T>;
+template < class T > // добавить перед
+BiList< T >* add(BiList< T >* h, const T& v)
+{
+  BiList< T >* e = new BiList< T >;
   e->val = v;
   e->prev = h->prev;
   e->next = h;
@@ -23,15 +26,16 @@ BiList<T> *add(BiList<T> *h, const T &v) {
   return e;
 }
 
-template <class T> // добавить после
-BiList<T> *insert(BiList<T> *h, const T &v) {
+template < class T > // добавить после
+BiList< T >* insert(BiList< T >* h, const T& v)
+{
 
-  BiList<T> *tmp = nullptr;
+  BiList< T >* tmp = nullptr;
   if (h->next) {
     tmp = add(h->next, v);
 
   } else {
-    tmp = new BiList<T>;
+    tmp = new BiList< T >;
     tmp->val = v;
     tmp->prev = h;
     tmp->next = nullptr;
@@ -40,10 +44,12 @@ BiList<T> *insert(BiList<T> *h, const T &v) {
   return tmp;
 }
 
-template <class T> BiList<T> *fake(BiList<T> *h) {
+template < class T >
+BiList< T >* fake(BiList< T >* h)
+{
   // BiList< T >* r = static_cast< BiList< T >* >(::operator new(sizeof(BiList<
   // T >*)));
-  BiList<T> *f = new BiList<T>;
+  BiList< T >* f = new BiList< T >;
 
   f->prev = nullptr;
   f->next = h;
@@ -55,21 +61,26 @@ template <class T> BiList<T> *fake(BiList<T> *h) {
   return f;
 }
 
-template <class T> BiList<T> *make(const T &data) {
-  BiList<T> *h = new BiList<T>{data, nullptr, nullptr};
+template < class T >
+BiList< T >* make(const T& data)
+{
+  BiList< T >* h = new BiList< T >{data, nullptr, nullptr};
   return fake(h);
 }
 
-template <class T> BiList<T> *rmfake(BiList<T> *h) noexcept {
-  BiList<T> *ret = h->next;
+template < class T >
+BiList< T >* rmfake(BiList< T >* h) noexcept
+{
+  BiList< T >* ret = h->next;
   delete h;
   return ret;
 }
 
-template <class T> // удалить элемент, который передается
-BiList<T> *cut(BiList<T> *node) noexcept {
-  BiList<T> *next = node->next;
-  BiList<T> *prev = node->prev;
+template < class T > // удалить элемент, который передается
+BiList< T >* cut(BiList< T >* node) noexcept
+{
+  BiList< T >* next = node->next;
+  BiList< T >* prev = node->prev;
 
   if (prev) {
     prev->next = next;
@@ -82,24 +93,28 @@ BiList<T> *cut(BiList<T> *node) noexcept {
   return next;
 }
 
-template <class T> // удалить после
-BiList<T> *erase(BiList<T> *h) noexcept {
+template < class T > // удалить после
+BiList< T >* erase(BiList< T >* h) noexcept
+{
   return h->next = cut(h->next);
 }
 
-template <class T> // очистить
-BiList<T> *clear(BiList<T> *h, BiList<T> *e) noexcept {
+template < class T > // очистить
+BiList< T >* clear(BiList< T >* h, BiList< T >* e) noexcept
+{
   while (h != e) {
     h = cut(h);
   }
   return h;
 }
 
-template <class T> void print(BiList<T> *fake) noexcept {
+template < class T >
+void print(BiList< T >* fake) noexcept
+{
   std::cout << "Forward: ";
 
-  BiList<T> *cur = fake->next;
-  BiList<T> *tail = nullptr;
+  BiList< T >* cur = fake->next;
+  BiList< T >* tail = nullptr;
 
   while (cur) {
     std::cout << cur->val << " ";
--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "interface.hpp"
 #include <iostream>
-int main() {
-  BiList<int> *fake = make(10);   // list — это fake-заголовок!
-  BiList<int> *list = fake->next; // list — это fake-заголовок!
+int main()
+{
+  BiList< int >* fake = make(10);
+  BiList< int >* list = fake->next;
 
   add(list, 5); // добавляем перед первым реальным элементом
   add(list, 3);
   insert(list, 15); // добавляем после первого элемента
-  
+
   print(fake); // передаём fake-узел
 
   list = rmfake(fake);
